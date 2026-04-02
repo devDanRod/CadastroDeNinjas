bebce1b12884e0471a743beb75023ae147b2c33f
daniel.nascimento <danielrod205@gmail.com>
2026-04-02 16:36:00 -0300

    Adicionando. nova dependencia JPA e criei um ninja model que é  trasnformando uma classe em entidade utilizando muito anottation

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,60 @@
+package dev.java10x.CadastroDeNinjas;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import jakarta.persistence.*;
+
+// Entity vai transformar a classe em uma tabela
+@Entity
+@Table(name = "tb_cadastro")
+
+public class NinjaModel {
+    @Id// anotação para  que atributo logo abaixo sera o id do banco
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // gera que o atributo seja auto-incremente
+    private long id;
+    private String nome;
+    private int idade;
+    private String email;
+
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "NinjaModel{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
