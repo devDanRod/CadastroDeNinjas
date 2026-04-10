d341365ef008646dcafc4d2745c063dbb45b5314
daniel.nascimento <danielrod205@gmail.com>
2026-04-10 09:10:54 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -21,47 +21,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "NinjaModel{" +
-                "nome='" + nome + '\'' +
-                ", idade=" + idade +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
